702538e93e8cd2724932f59ae52b2d7f425cc4f1
Chathuranga <nchathu2014@gmail.com>
2021-07-31 18:21:13 +0530

    Validation

--- validations.js
+++ validations.js
@@ -1,17 +1,64 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/mongo-exercises')
     .then(() => console.log('Conected to the MongoDB...'))
     .catch((err) => console.log('Error to connect MongoDB...', err));
 
 // Create course Schema
-
+/*
+Validation 1: name property is required
+Validation 2: 'price' is required when 'isPublished' true 
+Validation 3: Custom validations on 'tags'
+Validation 4: Async validation
+*/
 const courseSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 100,
+        // /pattern/
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['web', 'mobile', 'network']
+    },
     author: String,
     isPublished: Boolean,
-    tags: [String],
-    date: { type: Date, default: Date.now }
+    tags: {
+        type: Array,
+        validate: {
+            validator: function (v) {
+                return v && v.length > 0
+            },
+            message: 'A value should have at least one tag'
+        }
+    },
+    tags_async: {
+        type: Array,
+        validate: {
+            isAsync: true,
+            validator: function (v, callback) {
+                setTimeout(() => {
+                    //Do some async work
+                    const result = v && v.length > 0;
+                    callback(result);
+                }, 4000)
+            },
+            message: 'A value should have at least one tag ###'
+        }
+
+    },
+    date: { type: Date, default: Date.now },
+    price: {
+        type: Number,
+        min: 10,
+        max: 150,
+        required: function () {
+            return this.isPublished;
+        }
+    }
 });
 
 /*
@@ -26,10 +73,13 @@ const Course = mongoose.model('Course', courseSchema); //Class
 
 async function createCourse() {
     const course = new Course({// Instance
-        //name: 'Angular course',
+        name: 'Angular and Web',
         author: 'Nuwan',
         isPublished: true,
-        tags: ['angular', 'front-end']
+        tags: ['angular'],
+        price: 19,
+        category: 'web',
+        tags_async: []
     });
 
     try {
@@ -41,3 +91,7 @@ async function createCourse() {
 }
 
 createCourse();
+
+
+
+
